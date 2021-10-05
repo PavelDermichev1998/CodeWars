d7ed7c56d64c37c360e0eed8d1f16bd23967c7db
PavelDermichev1998 <83948999+PavelDermichev1998@users.noreply.github.com>
2021-10-05 22:58:44 +0300

    Create cod1.js
    Given an array of integers.
    
    Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers.
    
    If the input array is empty or null, return an empty array.
    
    Example
    For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].

---
+++ cod1.js
@@ -0,0 +1,23 @@
+function countPositivesSumNegatives(input) {
+
+if (input == null || input.length < 1){
+  return [];
+}
+
+var newArray = [0, 0];
+
+
+for (var i = 0; i < input.length; i++){
+
+  if (input[i] > 0)
+    {
+    newArray[0] += 1;
+    }
+
+  else {
+    newArray[1] += input[i];
+  }
+
+  }
+return newArray;
+}
