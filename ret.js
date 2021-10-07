0fc94d276c8ff9c48678853c6899f66d85f0c7cc
PavelDermichev1998 <83948999+PavelDermichev1998@users.noreply.github.com>
2021-10-07 19:42:06 +0300

    Update ret.js

--- ret.js
+++ ret.js
@@ -9,3 +9,11 @@ function fakeBin(x){
   }
   return el
 }
+//////////////////////////
+function fakeBin(x){
+  let el = '';
+  for (let i = 0; i < x.length; i++) {
+    (x[i] < 5) ? el+=0 : el+=1
+  }
+  return el
+}
