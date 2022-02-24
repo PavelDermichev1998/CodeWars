353797f27acb5d166517fa237efe87bafce424eb
PavelDermichev1998 <83948999+PavelDermichev1998@users.noreply.github.com>
2022-02-24 20:49:43 +0300

    Update 4.js

--- 4.js
+++ 4.js
@@ -2,7 +2,6 @@ function toCamelCase (str) {
 
     str = String (str);
     str = Array.from (str);
-
         for (let i = 0; i < str.length; i++) {
             if (str[i] == "_" || str[i] == "-") {
                     str[i+1] = str[i+1].toUpperCase();
