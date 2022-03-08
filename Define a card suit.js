70c1dde743b56e250beeeedf95dd0fd8f2f076b0
PavelDermichev1998 <83948999+PavelDermichev1998@users.noreply.github.com>
2022-03-08 19:04:38 +0300

    Update Define a card suit.js

--- Define a card suit.js
+++ Define a card suit.js
@@ -1,6 +1,7 @@
 function defineSuit(card) {
   for (let i = 0; i < card.length; i++){
     if (card[i] == "♣") {
+      
       return 'clubs'
     } else if (card[i] == "♦"){
       return 'diamonds'
